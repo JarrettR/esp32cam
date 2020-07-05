730c8528a8bc4fcd286e48f594db298d527b8fd8
Mariano Dupont <mariano@mipscom.com.ar>
2020-07-05 15:49:13 -0300

    Possibility to set various parameters

--- src/esp32cam.cpp
+++ src/esp32cam.cpp
@@ -39,6 +39,240 @@ CameraClass::changeResolution(const Resolution& resolution, int sleepFor)
   return true;
 }
 
+bool
+CameraClass::changeResolution(const Resolution& resolution, int sleepFor)
+{
+  sensor_t* sensor = esp_camera_sensor_get();
+  if (sensor == nullptr) {
+    return false;
+  }
+
+  framesize_t frameSize = resolution.as<framesize_t>();
+  if (sensor->status.framesize == frameSize) {
+    return true;
+  }
+
+  if (sensor->set_framesize(sensor, frameSize) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+
+bool
+CameraClass::setAutoGain(bool enabled, int sleepFor = 500)
+{
+  sensor_t* sensor = esp_camera_sensor_get();
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_gain_ctrl(sensor, int(enabled)) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+bool
+CameraClass::setAutoExposure(bool enabled, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_exposure_ctrl(sensor, int(enabled)) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+bool
+CameraClass::setGain(int gain, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_agc_gain(sensor, gain) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+
+bool
+CameraClass::setExposure(int exp, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_aec_value(sensor, exp) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+bool
+CameraClass::setVerticalFlip(bool flip, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_vflip(sensor, int(flip)) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+
+bool
+CameraClass::setQuality(int quality, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_quality(sensor, quality) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+bool
+CameraClass::setGainCeiling(int ceiling, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  int ceil = 0;
+  switch (ceiling) {
+    case 2:
+      ceil = GAINCEILING_2X;
+      break;
+    case 4:
+      ceil = GAINCEILING_4X;
+      break;
+    case 8:
+      ceil = GAINCEILING_8X;
+      break;
+    case 16:
+      ceil = GAINCEILING_16X;
+      break;
+    case 32:
+      ceil = GAINCEILING_32X;
+      break;
+    case 64:
+      ceil = GAINCEILING_64X;
+      break;
+    case 128:
+      ceil = GAINCEILING_128X;
+      break;
+    default:
+      return false;
+  }
+  if (sensor->set_gainceiling(sensor, ceil) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+
+bool
+CameraClass::setBrightness(int brightness, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_brightness(sensor, brightness) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+bool
+CameraClass::setLensCorrection(bool enabled, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_lenc(sensor, int(enabled)) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+
+bool
+CameraClass::setSaturation(int saturation, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_saturation(sensor, saturation) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+bool
+CameraClass::setContrast(int contrast, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_contrast(sensor, contrast) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+
+bool
+CameraClass::setSharpness(int sharpness, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_sharpness(sensor, sharpness != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+bool
+CameraClass::setHorizontalFlip(bool enabled, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_hmirror(sensor, int(enabled)) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+
+bool
+CameraClass::setBlackPixelCorrection(bool enabled, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_bpc(sensor, int(enabled)) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+bool
+CameraClass::setWhitePixelCorrection(bool enabled, int sleepFor = 500)
+{
+  if (sensor == nullptr) {
+    return false;
+  }
+  if (sensor->set_wpc(sensor, int(enabled)) != 0) {
+    return false;
+  }
+  delay(sleepFor);
+  return true;
+}
+
 std::unique_ptr<Frame>
 CameraClass::capture()
 {
--- src/esp32cam.h
+++ src/esp32cam.h
@@ -13,26 +13,39 @@ class CameraClass
 public:
   /** \brief Enable camera.
    */
-  bool
-  begin(const Config& config);
+  bool begin(const Config& config);
 
   /** \brief Disable camera.
    */
-  bool
-  end();
+  bool end();
 
   /** \brief Change camera resolution.
    *  \pre Initial resolution is higher than specified resolution.
    *  \param resolution new resolution
    *  \param sleepFor how long to wait for stabilization (millis)
    */
-  bool
-  changeResolution(const Resolution& resolution, int sleepFor = 500);
+  bool changeResolution(const Resolution& resolution, int sleepFor = 500);
+
+  bool setAutoGain(bool enabled, int sleepFor = 500);
+  bool setAutoExposure(bool enabled, int sleepFor = 500);
+  bool setGain(int gain, int sleepFor = 500);    //  (0 - 30)
+  bool setExposure(int exp, int sleepFor = 500); // set exposure manually  (0-1200)
+  bool setVerticalFlip(bool flip, int sleepFor = 500);
+  bool setQuality(int quality, int sleepFor = 500); // (0 - 63)
+  bool setGainCeiling(int ceiling,
+                      int sleepFor = 500); // Image gain (x2, x4, x8, x16, x32, x64 or x128)
+  bool setBrightness(int brightness, int sleepFor = 500); // (-2 to 2)
+  bool setLensCorrection(bool enabled, int sleepFor = 500);
+  bool setSaturation(int saturation, int sleepFor = 500); // (-2 to 2)
+  bool setContrast(int contrast, int sleepFor = 500);     // (-2 to 2)
+  bool setSharpness(int sharpness, int sleepFor = 500);   // (-2 to 2)
+  bool setHorizontalFlip(bool enabled, int sleepFor = 500);
+  bool setBlackPixelCorrection(bool enabled, int sleepFor = 500);
+  bool setWhitePixelCorrection(bool enabled, int sleepFor = 500);
 
   /** \brief Capture a frame of picture.
    */
-  std::unique_ptr<Frame>
-  capture();
+  std::unique_ptr<Frame> capture();
 
   struct StreamMjpegConfig
   {
@@ -48,11 +61,9 @@ public:
    *  \pre The camera has been initialized to JPEG mode.
    *  \return number of frames streamed.
    */
-  int
-  streamMjpeg(Client& client, const StreamMjpegConfig& cfg);
+  int streamMjpeg(Client& client, const StreamMjpegConfig& cfg);
 
-  int
-  streamMjpeg(Client& client)
+  int streamMjpeg(Client& client)
   {
     return streamMjpeg(client, StreamMjpegConfig());
   }
